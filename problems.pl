8cafc4eadf95e1f7adbe062528e04f6770e74d98
DoaaAli11 <doali246885@gmail.com>
2024-03-20 00:14:27 +0200

    P1 Done

--- problems.pl
+++ problems.pl
@@ -3,11 +3,49 @@
 :- dynamic alternative/2.
 :- dynamic boycott_company/2.
 
+%_____________________________________________________________
+
+%append lists
+
+concat([], L2, L2).    
+
+concat([H | T], L2, [H | R]) :-
+	concat(T, L2, R).
+
+
+%insert element at the end
+push_back(L, E, R):-
+	concat(L, [E], R).
+
+%insert element at the beg
+push_front(L, E, R):-
+	concat([E], L, R).
+
+%_____________________________________________________________
+
+list_orders(CustomerName, AllOrders) :-
+	list_orders(CustomerName, 1, [], AllOrders).
+
+list_orders(CustomerName, OrderNum, Orders, AllOrders) :-
+	customer(CustomerID, CustomerName),
+    	order(CustomerID, OrderNum, Items),
+    	NextOrderNum is OrderNum + 1,
+	push_front(Orders, order(CustomerID, OrderNum, Items), NewAllOrders),
+    	list_orders(CustomerName, NextOrderNum, NewAllOrders, AllOrders).
+
+list_orders(_, _, Orders, Orders). 
+
+%_____________________________________________________________
+
+
 % problem 3
 getItemsInOrderById(CustomerUsername, OrderID, Items) :-
 	customer(CustomerID, CustomerUsername),
 	order(CustomerID, OrderID, Items).
 % problem 3 Done
+
+%_____________________________________________________________
+
 
 % problem 11
 getTheDifferenceInPriceBetweenItemAndAlternative(BoycottItem, Alt, DiffPrice):-
@@ -16,6 +54,8 @@ getTheDifferenceInPriceBetweenItemAndAlternative(BoycottItem, Alt, DiffPrice):-
 	item(BoycottItem, _, BoycottPrice),
 	DiffPrice is BoycottPrice - AltPrice.
 % problem 11 Done
+
+%_____________________________________________________________
 
 
 % Bonus
